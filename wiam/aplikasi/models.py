0415e979f7717c4f3eb1d5d8c2012887525a0b9d
wiam <wiamwulandari10@gmail.com>
2024-03-22 21:10:14 +0700

    buku

--- wiam/aplikasi/models.py
+++ wiam/aplikasi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class bukuku (models.Model):
+    nama = models.CharField(max_length=50)
+    npm = models.CharField(max_length=50)
+    prodi = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return '{}. {}'. format(self.id,self.nama)
--- wiam/wiam/settings.py
+++ wiam/wiam/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'aplikasi',
 
 ]
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'wiam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'data_buku',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
         
     }
 }
